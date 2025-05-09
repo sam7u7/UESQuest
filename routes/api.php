5feffc1b586ca7f2fb42b957d75fab7588008821
sam7u7 <ba18048@ues.edu.sv>
2025-05-08 20:14:37 -0600

    creacion controller y modelo grupometa

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RolController;
+use App\Http\Controllers\Api\GrupoMetaController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
