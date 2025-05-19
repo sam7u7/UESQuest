5848e57bc99e57113f08b43b0f855e59659de79b
sam7u7 <ba18048@ues.edu.sv>
2025-05-18 18:52:13 -0600

    agregando rutas a archivo api

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,19 @@
 <?php
 
+use App\Http\Controllers\Api\EncuestaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RolController;
 use App\Http\Controllers\Api\GrupoMetaController;
+use App\Http\Controllers\Api\RealizaEncuestaController;
+use App\Http\Controllers\Api\RespuestaUsuarioController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::apiResource('/roles', RolController::class);
+Route::apiResource('/encuestas', EncuestaController::class);
+Route::apiResource('/encuestaRealizada', RealizaEncuestaController::class);
+Route::apiResource('/respuestasUsuario', RespuestaUsuarioController::class);
 //Route::post('/roles',[RolController::class,'store']);
